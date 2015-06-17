28a6427c04ff66683d39bf0d393b0257f6679ee0
asosnovsky <arielsosnovsky@gmail.com>
2015-06-16 21:29:21 -0400

    new framework

--- js/main.js
+++ js/main.js
@@ -1,58 +1,42 @@
 'use strict';
-
-StockRender.AppRender.register({
-	id: "49e90eee6ce1942a94136fc8db19319c",
-	name: "Tables",
-	version: "1.0.0",
-	defaults: {
-		terminal: {
-			x: 0,
-			y: 0,
-			w: 100,
-			h: 100
-		}
-	},
-	beforeRender: function () {
-		console.log('running beforeRender!');
-	},
-	ready: function(AppMemory, AppData) {
-		/*Defining Variables*/
-		var last_input;
-
-		/*Reading User-Data*/
-		AppMemory.read('last_input')
-			.success(function(data){
-				if(!data) {
-					AppMemory.write('last_input','A');
-					last_input = 'A';
-				} else {
-					last_input = data;
-				}
-			})
-			.error(function(err, data){
-				if(err) {
-					console.log('AppMemory not retrieved',data);
-					AppMemory.write('last_input','A');
-					last_input = 'A';
-				}
-			});
-
-		/*Running Program*/
-		Runner.loadData(AppData,last_input);
-
-		/*Setting click events*/
-		$('#ticker-btn').click(function(){
-			Runner.toggleOverhead();					
-			Runner.loadData(AppData, $('#stock')[0].value);
-			return;
-		});
-
-		$('#stock').keypress(function(e){
-			if( e.which === 13 ) {
-				Runner.toggleOverhead();
-				Runner.loadData(AppData, $('#stock')[0].value);
-				return;
-			}
-		})
-	}
+// Containers
+var dt = {data:{},list:{id:{},ticker:{}},fail:{"1":{},"2":{}}};
+/*Direct Data*/
+SR.AppData.v1.direct.GET('AAPL','epsbase').then(function (data) {
+	dt.data = data.response;
+	$('.datasets').html(JSON.stringify(dt.data,undefined, 2	));
+}, function (error) {
+	alert("Opps I failed...");
+	console.log(error);
+});
+/*Id list*/
+SR.AppData.v1.idlist.GET('json').then(function (data) {
+	dt.list.id = data.response;
+	$('.idlist').html(JSON.stringify(dt.list.id,undefined, 2	));
+}, function (error) {
+	alert("Opps I failed...");
+	console.log(error);
+});
+/*Ticker List*/
+SR.AppData.v1.Tickerlist.GET('array').then(function (data) {
+	dt.list.ticker = data.response;
+	$('.tickerlist').html(JSON.stringify(dt.list.ticker,undefined, 2	));
+}, function (error) {
+	alert("Opps I failed...");
+	console.log(error);
+});
+/*Errors*/
+/*1*/
+SR.AppData.v1.direct.GET('AAPL','data').then(function (data) {
+	alert("Opps I didn't fail...");
+}, function (error) {
+	dt.fail["1"] = error;
+	$('.errorData-1').html(JSON.stringify(dt.fail["1"],undefined, 2	));
+});
+/*2*/
+SR.AppData.v1.direct.GET('AAPL','pricedata').then(function (data) {
+	alert("Opps I didn't fail...");
+}, function (error) {
+	dt.fail["2"] = error;
+	$('.errorData-2').html(JSON.stringify(dt.fail["2"],undefined, 2	));
 });
